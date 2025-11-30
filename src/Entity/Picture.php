5d4e3d64046e8d867263231cb1a6307d84c9bde4
oumayma-naffeti <naffeti.oumayma.pro@gmail.com>
2025-11-30 15:48:17 +0100

    sec update

--- src/Entity/Picture.php
+++ src/Entity/Picture.php
@@ -4,7 +4,6 @@ namespace App\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\HttpFoundation\File\File;
-use Vich\UploaderBundle\Mapping\Annotation\Uploadable;
 use Vich\UploaderBundle\Mapping\Annotation as Vich;
 use Symfony\Component\Validator\Constraints as Assert;
 
@@ -19,19 +18,21 @@ class Picture
 
     #[ORM\Column(type: 'string', length: 255)]
     private ?string $filename = null;
-    
+
     /**
      * @Vich\UploadableField(mapping="property_image", fileNameProperty="filename")
      * @Assert\Image(
      *      mimeTypes = "image/jpeg"
      * )
-     * @var File|null
      */
     private ?File $imageFile = null;
 
     #[ORM\ManyToOne(targetEntity: Property::class, inversedBy: 'pictures')]
     #[ORM\JoinColumn(nullable: false)]
     private ?Property $property = null;
+
+    #[ORM\Column(type: 'datetime')]
+    private ?\DateTimeInterface $updatedAt = null;
 
     public function getId(): ?int
     {
@@ -46,25 +47,22 @@ class Picture
     public function setFilename(?string $filename): self
     {
         $this->filename = $filename;
-
         return $this;
     }
 
-    /**
-     * @return File|null
-     */
     public function getImageFile(): ?File
     {
         return $this->imageFile;
     }
 
-    /**
-    * @param File $imageFile
-    * @return self
-    */
-    public function setImageFile(File $imageFile): self
+    public function setImageFile(?File $imageFile = null): self
     {
         $this->imageFile = $imageFile;
+
+        if ($imageFile !== null) {
+            // Obligatoire pour déclencher l'upload dans VichUploader
+            $this->updatedAt = new \DateTimeImmutable();
+        }
 
         return $this;
     }
@@ -77,7 +75,17 @@ class Picture
     public function setProperty(?Property $property): self
     {
         $this->property = $property;
+        return $this;
+    }
 
+    public function getUpdatedAt(): ?\DateTimeInterface
+    {
+        return $this->updatedAt;
+    }
+
+    public function setUpdatedAt(\DateTimeInterface $updatedAt): self
+    {
+        $this->updatedAt = $updatedAt;
         return $this;
     }
 }
